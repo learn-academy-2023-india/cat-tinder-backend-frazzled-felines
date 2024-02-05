215f540eb20b6f48989c5230dd66fad5a7e71000
jwat5623 <jamar34watson56@gmail.com>
2024-02-05 10:53:56 -0800

    adds backend structure

---
+++ config/initializers/cors.rb
@@ -0,0 +1,14 @@
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000/'  # <- change this to allow requests from any domain while in development.
+  
+      resource 'http://localhost:3000/',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
