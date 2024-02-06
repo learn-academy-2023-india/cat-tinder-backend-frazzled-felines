5b5f24597fa03a02451c6b388aa34cd29c947a02
Jamar Watson <jamar34watson56@gmail.com>
2024-02-05 16:42:16 -0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,4 +11,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
-  end
+end
